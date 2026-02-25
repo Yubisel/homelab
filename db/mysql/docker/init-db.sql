c3a5818cfb856a9e5a0a16ea34a81fb630fd7a5a
Yubisel Vega A. <yubiselv@gmail.com>
2026-02-25 04:31:06 +0000

    feat: add custom MySQL configuration and initialization script for Docker setup

---
+++ db/mysql/docker/init-db.sql
@@ -0,0 +1,6 @@
+-- Initialization script for MySQL (runs on first start)
+-- Default database and user are created from MYSQL_* env vars by the image.
+-- Add any extra databases or statements below.
+
+-- USE default_db;
+-- SHOW DATABASES;
